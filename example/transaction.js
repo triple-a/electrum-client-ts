29ed2287f33876054ab7d951b3e14b59f24318cb
Steve Guo <steve.guo@triple-a.io>
2023-08-15 12:22:25 +0800

    Update examples.

--- example/transaction.js
+++ example/transaction.js
@@ -1,20 +1,17 @@
 const { ElectrumClient } = require('../dist');
 
 async function main() {
-  const client = new ElectrumClient('fortress.qtornado.com', 50002, 'ssl');
+  const client = new ElectrumClient('electrum.bitaroo.net', 50002, 'ssl');
 
   try {
     console.log('connecting...');
-    await client.connect(
-      'electrum-client-js', // optional client name
-      '1.4.2', // optional protocol version
-    );
+    await client.connect();
 
     const tx = await client.blockchain_transaction_get(
       '2fdee6050a8713d70ffc823771e14d2cbc75f4902282f167f5ee1cf3f3962d3a',
       true,
     );
-    console.log('Tx: %j', tx);
+    console.log('Tx: %o', tx);
 
     await client.close();
   } catch (err) {
--- example/transaction.ts
+++ example/transaction.ts
@@ -1,14 +1,14 @@
 import { ElectrumClient } from '../src';
 
 const main = async () => {
-  const ecl = new ElectrumClient('btc.smsys.me', 50001, 'tcp');
+  const ecl = new ElectrumClient('fortress.qtornado.com', 50002, 'ssl');
   await ecl.connect();
   const tx = await ecl.blockchain_transaction_get(
     '2fdee6050a8713d70ffc823771e14d2cbc75f4902282f167f5ee1cf3f3962d3a',
     true,
   );
 
-  console.log(tx);
+  console.log('tx: %o', tx);
 
   ecl.close();
 };
