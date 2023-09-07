945cc41f7fa606cfaf672dc404499c8d9ca19d44
Steve Guo <steve.guo@triple-a.io>
2023-09-07 14:19:39 +0800

    Update example/scripthash-detailed-history.ts.

--- example/scripthash-detailed-history.ts
+++ example/scripthash-detailed-history.ts
@@ -11,9 +11,14 @@ const main = async () => {
 
     const scriptHash =
       process.argv[2] ||
-      '740485f380ff6379d11ef6fe7d7cdd68aea7f8bd0d953d9fdf3531fb7d531833';
+      //'740485f380ff6379d11ef6fe7d7cdd68aea7f8bd0d953d9fdf3531fb7d531833';
+      //'8b01df4e368ea28f8dc0423bcf7a4923e3a12d307c875e47a0cfbf90b5c39161';
+      'cf52d58a518fce3b9d8ff7a76c07f1a1c838d4483d4ddf95ec781244096cd9b2';
 
-    const address = process.argv[3] || '1LpErkYJUeGkTiJ68HEfg6p4T1rpYXRNLX';
+    const address =
+      process.argv[3] || //'1LpErkYJUeGkTiJ68HEfg6p4T1rpYXRNLX';
+      //'1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa';
+      '1Ep2wRnRj2tPn77czVzk5XRLau9zt8Syt';
 
     const detailedHistory = await client.getScriptHashDetailedHistory(
       scriptHash,
